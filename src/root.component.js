da93c02da172f12abf96fa8a7e2408e6f28a1a27
Anand raja <meetanandns@gmail.com>
2022-07-30 19:52:04 +0000

    workable

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './root.component.module.css';
+
+export default function Root(props) {
+  return (
+    <div className={classes.container}>
+      Page Not Found!
+    </div>
+  );
+}
